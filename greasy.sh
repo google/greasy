42e93412889ac547153295228b5b8dfb0c4a0c1a
J Pratt <jp10010101010000@gmail.com>
2022-01-13 11:54:52 +1100

    Use https://github.com/mbrukman/autogen to re-generate license

--- greasy.sh
+++ greasy.sh
@@ -1,4 +1,4 @@
-# Git Rebase (and run) Easy (GREasy)
+#!/bin/bash -eu
 #
 # Copyright 2022 Google LLC
 #
@@ -6,13 +6,15 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    https://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Git Rebase (and run) Easy (GREasy)
 
 # Installs Chrome's very handy depot management tools.
 # https://dev.chromium.org/developers/how-tos/install-depot-tools
